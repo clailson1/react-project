1d9a8eadf64921cb7013a69ab4f1ab6bad6a5c75
Clailson <gadelhaclailson@gmail.com>
2025-04-30 18:46:58 -0300

    Criação da pasta layouts e do arquivo PageLayout.jsx. Remoção dos componentes Header e Footer do arquivo App.jsx. Adição dos componentes Header e Footer e Outlet, que renderiza o conteúdo da página no PageLayout.jsx. Alteração no arquivo Paths.jsx colocando o Route PageLayout e dentro dele as rotas para as páginas Home, About, e Contact.

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,10 @@
-import Footer from "./components/Footer";
-import Header from "./components/Header";
-import Home from "./pages/Home";
 import Paths from "./routes/Paths";
 
 const App = () => {
   return (  
     // parte visível do componente
     <>
-      <Header />
-        <Paths />
-      <Footer />
+      <Paths />
     </>
   );
 }
--- src/routes/Paths.jsx
+++ src/routes/Paths.jsx
@@ -2,16 +2,19 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import About from "../pages/About";
 import Contact from "../pages/Contact";
 import Home from "../pages/Home";
+import PageLayout from "../layouts/PageLayout";
 
 const Paths = () => {
     return (  
         <>
             <BrowserRouter>
                 <Routes>
-                    <Route path="/" element={<Home />}/>
-                    <Route path="/home" element={<Home />}/>
-                    <Route path="/about" element={<About />}/>
-                    <Route path="/contact" element={<Contact />}/>
+                    <Route path="/" element={<PageLayout />}>
+                        <Route index element={<Home />}/>
+                        <Route path="/home" element={<Home />}/>
+                        <Route path="/about" element={<About />}/>
+                        <Route path="/contact" element={<Contact />}/>
+                    </Route>
                 </Routes>
             </BrowserRouter>
         </>
