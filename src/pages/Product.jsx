11a5cbb64cb70bd6fc31cde588cabe8d41cb8b10
Clailson <gadelhaclailson@gmail.com>
2025-05-03 20:02:59 -0300

    Esilização do Product com uso do Panel do PrimeReact
    Estilização do Product com PrimeReact, e estilização do NavLink para ser um botão.

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,15 +1,31 @@
 import { NavLink, useParams } from "react-router-dom";
+import { Panel } from 'primereact/panel';
 
 const Product = () => {
-
     const { id, name } = useParams();
-    return (   
-        <>
-            <NavLink to={"/products"}>Back to list of products</NavLink>
-            <h2>{name}</h2>
-            <h3>Product id: {id}</h3>
-        </>
+
+    return (
+        <div className="m-5">
+            <div className="card">
+                <NavLink
+                    to="/products"
+                    className="p-button p-component no-underline"
+                    style={{ display: 'inline-flex', alignItems: 'center', justifyContent: 'center' }}
+                >
+                    <span className="p-button-icon pi pi-arrow-left mr-2" />
+                    <span className="p-button-label">Back to list of products</span>
+                </NavLink>
+            </div>
+            <div className="mt-5">
+                <Panel header={name}>
+                    <p className="m-0">
+                        Product id: {id}
+                    </p>
+                </Panel>
+            </div>
+            
+        </div>
     );
 }
- 
-export default Product; 
+
+export default Product;
