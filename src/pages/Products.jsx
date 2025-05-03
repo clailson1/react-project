a650690757e5850475f402d033be249c6a1385c3
Clailson <gadelhaclailson@gmail.com>
2025-05-03 20:27:22 -0300

    Esilização da Products com uso da ListBox do PrimeReact
    Estilização da Products com PrimeReact, usando o ListBox. Usar o ListBox substitui o uso do NavLink para redirecionar as rotas para as páginas correspondentes usando a função handleNavigation.

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,17 +1,39 @@
-import { NavLink } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+
+import { ListBox } from 'primereact/listbox';
+import { useState } from "react";
+        
 
 const Products = () => {
+    const navigate = useNavigate();
+    const [selectedPage, setSelectedPage] = useState(null);
+
+    const pages = [
+        { product: 'Strawberry', value: '/product/1/Strawberry' },
+        { product: 'Banana', value: '/product/2/Banana' },
+        { product: 'Apple', value: '/product/3/Apple' },
+        { product: 'Grape', value: '/product/4/Grape' },
+        { product: 'Orange', value: '/product/5/Orange' },
+    ];
+
+    const handleNavigation = (e) => {
+        setSelectedPage(e.value);
+        navigate(e.value); // redireciona para a rota
+    };
+
     return (  
-        <>      
-            <h2>List of Products</h2>
-            <ul>
-                <li><NavLink to={"/product/1/Strawberry"}>Strawberry</NavLink></li>
-                <li><NavLink to={"/product/2/Banana"}>Banana</NavLink></li>
-                <li><NavLink to={"/product/3/Apple"}>Apple</NavLink></li>
-                <li><NavLink to={"/product/4/Grape"}>Grape</NavLink></li>
-                <li><NavLink to={"/product/5/Orange"}>Orange</NavLink></li>
-            </ul>
-        </>
+        <div className="m-5">      
+            <h2 className="flex justify-content-center">List of Products</h2>
+            <div className="card mt-5">  
+                <ListBox 
+                    value={selectedPage} 
+                    options={pages} 
+                    onChange={handleNavigation} 
+                    optionLabel="product" 
+                    className="w-full md:w-14rem" 
+                />
+            </div>
+        </div>
     );
 }
  
