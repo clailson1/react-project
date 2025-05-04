01cb8c38b0879bc040efcfa0e86324127e5721c9
Clailson <gadelhaclailson@gmail.com>
2025-05-04 20:31:00 -0300

    Esilização do Contact e do About
    Esilização do Contact e do About. O Contact usa o Panel do Prime react e o About usa o Fieldset.

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,18 +1,60 @@
 import { NavLink } from "react-router-dom";
+import { Panel } from 'primereact/panel';
+import 'primeicons/primeicons.css';
 
 const Contact = () => {
-    return (  
-        <>
-            <h3>Contact Page</h3>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque reprehenderit error labore, asperiores ab, ducimus modi fuga obcaecati, tempore sequi eum est commodi maiores ipsa officia aperiam cupiditate repellat hic.</p>
-            <nav>
-                <ul>
-                    <li><NavLink to={"/home"}>Home</NavLink></li>
-                    <li><NavLink to={"/about"}>About</NavLink></li>
-                </ul>
-            </nav>
-        </>
+    return (
+        <Panel
+            header={
+                <span>
+                    <i className="pi pi-user" style={{ marginRight: '8px' }}></i>
+                    Contact
+                </span>
+            }
+            className="m-5"
+        >
+            <p className="m-0">
+                Email: email@email.com
+            </p>
+            <p className="mt-2">
+                Phone: (11) 98888-7777
+            </p>
+            <div className="card flex justify-content-around mt-3">
+                <NavLink
+                    to={"/home"}
+                    className="p-button p-component no-underline"
+                    style={{ display: 'inline-flex', alignItems: 'center', justifyContent: 'center' }}
+                >
+                    <span
+                        className="p-button-icon pi pi-home mr-2"
+                        style={{ color: "black" }}
+                    />
+                    <span
+                        className="p-button-label"
+                        style={{ color: "black" }}
+                    >
+                        Back to Home
+                    </span>
+                </NavLink>
+                <NavLink
+                    to={"/about"}
+                    className="p-button p-component no-underline"
+                    style={{ display: 'inline-flex', alignItems: 'center', justifyContent: 'center' }}
+                >
+                    <span
+                        className="p-button-icon pi pi-user mr-2"
+                        style={{ color: "black" }}
+                    />
+                    <span
+                        className="p-button-label"
+                        style={{ color: "black" }}
+                    >
+                        About Page
+                    </span>
+                </NavLink>
+            </div>
+        </Panel>
     );
 }
- 
+
 export default Contact;
